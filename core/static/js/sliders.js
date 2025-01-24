9f84201504c8e180082cd1514aa24583ead45aa8
SirvavialTAG <alexyudin04@mail.ru>
2025-01-24 21:17:37 +1000

    Связка backend и frontend

---
+++ core/static/js/sliders.js
@@ -0,0 +1,91 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Инициализация Swiper для слайдера видеоклипов
+    const swiperVideo = new Swiper('.video_clip', {
+        loop: false, // Убрал loop из-за предупреждения
+        slidesPerView: 'auto',
+        spaceBetween: 40,
+        freeMode: true,
+        mousewheel: {
+            forceToAxis: true
+        },
+        wrapperClass: 'video-clip-wrapper',
+        slideClass: 'video-slide',
+        noSwiping: true
+    });
+
+    // Инициализация Swiper для слайдера изображений
+    const swiper = new Swiper('.image-slider', {
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+        },
+        autoheight: true,
+        slidesPerView: 'auto',
+        spaceBetween: 10,
+        freeMode: true,
+    });
+
+    const innerSliders = document.querySelectorAll('.image-slider__inner-slider');
+    const innerSwipers = {}; // Храним экземпляры вложенных слайдеров
+    innerSliders.forEach((slider, index) => {
+         innerSwipers[index] = new Swiper(slider, {
+            pagination: {
+             el: slider.querySelector('.image-slider__inner-pagination'),
+                clickable: true,
+            },
+           slidesPerView: 1,
+           loop: true,
+
+        });
+     });
+    const updateSlideBackground = (slide) => {
+        const imageContainer = slide.querySelector('.image-slider_image');
+        if (imageContainer) {
+            const imageUrl = imageContainer.getAttribute('data-background');
+            imageContainer.style.backgroundImage = `url('${imageUrl}')`;
+        }
+    };
+    swiper.on('slideChange', () => {
+         swiper.slides.forEach(slide => {
+          const imageContainer = slide.querySelector('.image-slider_image');
+              if(imageContainer){
+                imageContainer.style.backgroundImage = '';
+           }
+        });
+       const activeSlide = swiper.slides[swiper.activeIndex];
+          updateSlideBackground(activeSlide);
+
+          // Обновляем вложенный слайдер при смене активного слайда
+         const activeInnerSlider = activeSlide.querySelector('.image-slider__inner-slider');
+          if (activeInnerSlider) {
+              const index = Array.from(innerSliders).indexOf(activeInnerSlider);
+              innerSwipers[index].update(); //Обновляем Swiper на активном слайде
+          }
+    });
+    swiper.slides.forEach(slide => {
+        const imageContainer = slide.querySelector('.image-slider_image');
+            if(imageContainer){
+            const imageUrl = imageContainer.getAttribute('data-background');
+                imageContainer.style.backgroundImage = `url('${imageUrl}')`;
+            }
+    });
+    const buttons = document.querySelectorAll('.custom-open-pop-up');
+    const popUp = document.getElementById('custom-pop-up');
+    const popUpClose = document.getElementById('custom-pop-up-close');
+
+    if (buttons && popUp && popUpClose) {
+          buttons.forEach(button => {
+              button.addEventListener('click', function(e) {
+                  e.preventDefault();
+                  popUp.classList.add('active');
+              });
+         });
+
+         popUpClose.addEventListener('click', () => {
+             popUp.classList.remove('active');
+         });
+    }
+    else {
+     console.error("Один или несколько элементов pop-up не найдены!");
+    }
+});
