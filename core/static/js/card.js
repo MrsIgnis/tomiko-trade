981a82d9349dffe00f99578e30fb4fbd3bf7916c
SirvavialTAG <123053453+sirvavialtag@users.noreply.github.com>
2025-01-27 11:43:49 +1000

    Карточка автомобиля

---
+++ core/static/js/card.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // Инициализация Swiper
+    const swiper = new Swiper('.main-slider', {
+      autoheight: true,
+       slidesPerView: 'auto',
+        spaceBetween: 10,
+    });
+      const updateActiveThumbnail = () => {
+            thumbnails.forEach(thumbnail => {
+                thumbnail.classList.remove('active');
+            });
+           const activeThumbnail = document.querySelector(`.thumbnail-item[data-slide-index="${swiper.activeIndex}"]`);
+            if (activeThumbnail) {
+                activeThumbnail.classList.add('active');
+            }
+        };
+    const thumbnails = document.querySelectorAll('.thumbnail-item');
+     thumbnails.forEach(thumbnail => {
+        thumbnail.addEventListener('click', function() {
+           const slideIndex = parseInt(this.getAttribute('data-slide-index'));
+           swiper.slideTo(slideIndex);
+         });
+    });
+     const slidePrevButtons = document.querySelectorAll('.slider-button-prev');
+     slidePrevButtons.forEach(button => {
+         button.addEventListener('click', () => {
+            swiper.slidePrev();
+         });
+     });
+    const slideNextButtons = document.querySelectorAll('.slider-button-next');
+    slideNextButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            swiper.slideNext();
+       });
+    });
+    swiper.on('slideChange', () => {
+         updateActiveThumbnail();
+      });
+   updateActiveThumbnail();
+
+});
