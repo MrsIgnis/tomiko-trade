e4fea9326fe45344f4ba590a95dfe0da8edcf42b
Zakhar29 <125102570+Zakhar29@users.noreply.github.com>
2025-01-31 03:52:28 +1000

    Исправлены формы обратной связи в тех страничках, где было им плохо, прокрутка строчек сделана

---
+++ core/static/js/stocks.js
@@ -0,0 +1,24 @@
+ window.addEventListener('load', function() {
+        const popUp = document.getElementById('custom-pop-up');
+        const popUpClose = document.getElementById('custom-pop-up-close');
+
+        if (popUp && popUpClose) {
+           // Делегирование события клика на весь документ
+            document.addEventListener('click', function(event) {
+               if(event) {
+                   console.log(event.target);
+                     // Проверяем, был ли клик по элементу с классом custom-open-pop-up
+                       if (event.target.classList.contains('custom-open-pop-up')) {
+                           event.preventDefault(); // Предотвращаем стандартное поведение ссылки (если это ссылка)
+                             popUp.classList.add('active');
+                       }
+                   }
+            });
+
+            popUpClose.addEventListener('click', () => {
+                popUp.classList.remove('active');
+            });
+        } else {
+            console.error("Один или несколько элементов pop-up не найдены!");
+        }
+       });
